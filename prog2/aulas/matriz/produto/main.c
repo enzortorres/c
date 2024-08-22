38ce2903559aa450be58e80fa620c8616728f4b6
Enzo Ribas <135893033+enzortorres@users.noreply.github.com>
2024-08-22 14:37:20 +0000

    tde01

--- prog2/aulas/matriz/produto/main.c
+++ prog2/aulas/matriz/produto/main.c
@@ -8,7 +8,7 @@
 //     for(int i = 0; i < 3; i++) { // Pegar os valores da Matriz A
 //         for(int j = 0; j < 2; j++) {
 //             printf("Valor da %d linha e %d coluna da matriz A: ", i+1, j+1);
-//             scanf("%d", &matrizA[i][j]);
+//             sc anf("%d", &matrizA[i][j]);
 //         }
 //         printf("\n");
 //     }
--- prog2/tde 01/main.c
+++ prog2/tde 01/main.c
@@ -52,30 +52,37 @@
 //     return 0;
 // }
 
-#include <stdio.h>
+// #include <stdio.h> // EXERCÍCIO 3
+
+// int main(void) {
+//     int matrizA[2][5];
+//     int matrizResultante[5][2];
+
+//     for (int i = 0; i < 2; i++) {
+//         for (int j = 0; j < 5; j++) {
+//             printf("Valor da %d linha e %d coluna:", i+1, j+1);
+//             scanf("%d", &matrizA[i][j]);
+//         }
+//     }
+
+//     for (int i = 0; i < 5; i++) {
+//         for (int j = 0; j < 2; j++) {
+//             matrizResultante[i][j] = matrizA[j][i];
+//         }
+//     }
+
+//     for (int i = 0; i < 5; i++) {
+//         for (int j = 0; j < 2; j++) {
+//             printf("[ %d ]", matrizResultante[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h> // EXERCÍCIO 4
 
 int main(void) {
-    int matrizA[2][5];
-    int matrizResultante[5][2];
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 5; j++) {
-            printf("Valor da %d linha e %d coluna:", i+1, j+1);
-            scanf("%d", &matrizA[i][j]);
-        }
-    }
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 2; j++) {
-            matrizResultante[i][j] = matrizA[j][i];
-        }
-    }
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("[ %d ]", matrizResultante[i][j]);
-        }
-        printf("\n");
-    }
+    int matrizA[4][2], matrizB[2][4];
     return 0;
 }
