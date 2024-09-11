104c9626a7c357af8983a233d0819de422c8b1c2
Enzo Ribas <135893033+enzortorres@users.noreply.github.com>
2024-09-11 14:50:05 +0000

    aula 11/09

--- prog2/aulas/function/exercicios06/main.c
+++ prog2/aulas/function/exercicios06/main.c
@@ -19,20 +19,22 @@
 // }
 
 
+
+
 // #include <stdio.h> // EXERCICIO 2
 
-// int idade = 0;
 
-// void obrigadoAVotar(int idade);
+// void verificarVotacao(int idade);
 
 // int main(void) {
+//     int idade = 0;
 //     printf("Digite a sua idade: ");
 //     scanf("%d", &idade);
-//     obrigadoAVotar(idade);
+//     verificarVotacao(idade);
 //     return 0;
 // }
 
-// void obrigadoAVotar(int idade) {
+// void verificarVotacao(int idade) {
 //     if (idade >= 18 && idade <= 70) {
 //         printf("Voce eh obrigado a votar!");
 //     } else {
@@ -40,26 +42,88 @@
 //     }
 // }
 
+// #include <stdio.h> // EXERCÍCIO 3
+
+// int diasDeVida(int year, int month, int days);
+
+// int main(void) {
+//     int year = 0, month = 0, day = 0;
+
+//     printf("Digite quantos anos vc tem: ");
+//     scanf("%d", &year);
+//     printf("Digite quantos meses vc tem: ");
+//     scanf("%d", &month);
+//     printf("Digite quantos dias vc tem: ");
+//     scanf("%d", &day);
+//     printf("Voce tem %d dias de vida!", diasDeVida(year, month, day));
+// }
+
+// int diasDeVida(int years, int months, int days) {
+//     int totalDias = (years * 365) + (months * 30) + days;
+//     return totalDias;
+// }
 
 
-#include <stdio.h> // EXERCÍCIO 3
 
-int diasDeVida(int year, int month, int days);
+
+#include <stdio.h>
+
+float precoCamisa(char tamanho, char sexo);
 
 int main(void) {
-    int year = 0, month = 0, day = 0;
+    char sexo, tamanho;
+    float total = 0;
+    
+    printf("Digite o seu sexo: ");
+    scanf(" %c", &sexo);
+    printf("Digite o tamanho da camisa desejada: ");
+    scanf(" %c", &tamanho);
+    total = precoCamisa(tamanho, sexo);
+    if(total != 0) {
+        printf("O preço da camisa %c para o sexo %c é R$%.2f\n", tamanho, sexo, total);
+    } 
 
-    printf("Digite quantos anos vc tem: ");
-    scanf("%d", &year);
-    printf("Digite quantos meses vc tem: ");
-    scanf("%d", &month);
-    printf("Digite quantos dias vc tem: ");
-    scanf("%d", &day);
-    printf("Voce tem %d dias de vida!", diasDeVida(year, month, day));
+    return 0;
 }
 
-int diasDeVida(int years, int months, int days) {
-    int totalDias = (years * 365) + (months * 30) + days;
-    return totalDias;
+float precoCamisa(char tamanho, char sexo) {
+    float valor = 0;
+    if (sexo == 'M' || sexo == 'm') {
+        switch(tamanho) {
+            case 'p':
+            case 'P':
+                valor = 22.25;
+                break;
+            case 'm':
+            case 'M':
+                valor = 27.15;
+                break;
+            case 'g':
+            case 'G':
+                valor = 40;
+                break;
+            default:
+                printf("Tamanho digitado invalido!\n");
+        }
+    } else if (sexo == 'F' || sexo == 'f') {
+        switch(tamanho) {
+            case 'p':
+            case 'P':
+                valor = 28.75;
+                break;
+            case 'm':
+            case 'M':
+                valor = 39.99;
+                break;
+            case 'g':
+            case 'G':
+                valor = 51;
+                break;
+            default:
+                printf("Tamanho digitado invalido!\n");
+        }
+    } else {
+        printf("Sexo digitado invalido!\n");
+    }
+    return valor;
 }
-
